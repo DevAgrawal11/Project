bdce467256b048b1da5b0e3765f787e25862d060
DevAgrawal11 <dev.agrawal1109@gmail.com>
2023-10-05 22:50:43 +0530

    done

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +91 8767924238</a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i> +91 9465438354</a>
+         <a href="mailto:shakhanas@gmail.com"><i class="fas fa-envelope"></i> dev.agrawal1109@gmail.com</a>
+         <a href="mailto:anasbhai@gmail.com"><i class="fas fa-envelope"></i> harsh.agarwal@gmail.com</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Pune, India - 411046</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">Home</a>
+         <a href="#about">About</a>
+         <a href="bookings.php">My Bookings</a>
+         <a href="#reservation">Reservation</a>
+         <a href="#gallery">Gallery</a>
+         <a href="#contact">Contact</a>
+         <a href="#reviews">Reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="#">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="#">twitter <i class="fab fa-twitter"></i></a>
+         <a href="#">instagram <i class="fab fa-instagram"></i></a>
+         <a href="#">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="#">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2023 by Dev Agrawal | all rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
